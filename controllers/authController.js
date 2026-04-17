f1a90eeed51f82a137b2f63b816952b808559b11
RAJ SIDANA <ft.rajsidana@gmail.com>
2026-04-17 23:28:17 +0530

    added user authentication without jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,4 +36,34 @@ const register = async (req, res) => {
     });
 }
 
-export {register};
+// Login User
+
+    const login = async (req, res) => {
+        const { email, password } = req.body;
+
+        //check if your exists
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+
+        //check if password is correct
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                user: {
+                    id: user.id,
+                    email: user.email,
+                }
+            },
+        });
+    }
+
+export {register, login};
